ef960d575dd533161d8ff6c86bc94ef0a7b8f4b5
Alejandro <amankewitz@hotmail.com>
2024-10-11 02:46:05 -0300

    Work experience y foto perfil

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
 // Aquí puedes añadir funciones si necesitas interactividad
 console.log("Página de CV cargada");
+
+// Función para abrir el modal
+function openModal() {
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("modalImage");
+    var profileImg = document.querySelector(".profile-image");
+  
+    // Muestra el modal y establece la imagen en tamaño grande
+    modal.style.display = "flex";
+    modalImg.src = profileImg.src; // Asigna la misma imagen de perfil al modal
+  }
+  
+  // Función para cerrar el modal
+  function closeModal() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none"; // Oculta el modal
+  }
